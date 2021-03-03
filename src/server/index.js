743a2d117616e7f48fb31a17015ecf544f57ccaf
Robert Costello <robertcostello81@gmail.com>
2021-03-03 14:18:45 -0500

    worked on async await conversion

--- src/server/index.js
+++ src/server/index.js
@@ -9,66 +9,71 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/tasks', (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).send(tasks);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.get('/tasks/:id', (req, res) => {
-  Task.findById(req.params.id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send('Task not found.');
-      }
-      res.send(task);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findById(_id);
+    if (!task) return res.status(404).send('Task not found.');
+    res.status(200).send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const {description, completed} = req.body;
 
-  const task = new Task({description, completed});
-
-  task
-    .save()
-    .then(() => res.status(201).send(task))
-    .catch((e) => res.status(400).send(e));
+  try {
+    const task = new Task({description, completed});
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 app.get('/users', async (req, res) => {
-  User.find({})
-    .then((users) => res.status(200).send(users))
-    .catch((e) => res.status(500).send(e));
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.get('/users/:id', async (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send('User not found.');
-      }
-      res.send(user);
-    })
-    .catch((e) => res.status(500).send(e));
+  try {
+    const _id = req.params.id;
+    const user = await User.findById(_id);
+
+    if (!user) return res.status(404).send('User not found.');
+
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
 app.post('/users', async (req, res) => {
   const {name, email, password} = req.body;
   const user = new User({name, email, password});
-  user
-    .save()
-    .then(() => res.status(201).send(user))
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 app.listen(port, () => {
