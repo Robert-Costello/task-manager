88586c83bfdcb00cd9f1c9f304da8f7b99cf0e6c
Robert Costello <robertcostello81@gmail.com>
2021-03-08 18:54:28 -0500

    auth, logout, logoutAll funtionality

--- src/server/middleware/auth.js
+++ src/server/middleware/auth.js
@@ -9,8 +9,8 @@ const auth = async (req, res, next) => {
 
     if (!user) throw new Error();
 
+    req.token = token;
     req.user = user;
-
     next();
   } catch (error) {
     res.status(401).send({error: 'Please authenticate.'});
