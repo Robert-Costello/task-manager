e536e5d54d55eb19b98a8a72e855b1944b79b060
Robert Costello <robertcostello81@gmail.com>
2021-03-11 21:45:53 -0500

    added timestamps for users and tasks, added query filtering for tasks

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -5,45 +5,48 @@ const jwt = require('jsonwebtoken');
 const secret = require('../../../secrets');
 const Task = require('../models/task');
 
-const userSchema = mongoose.Schema({
-  name: {type: String, trim: true, required: true},
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    validate(val) {
-      if (!validator.isEmail(val)) throw new Error('Email is not valid');
+const userSchema = mongoose.Schema(
+  {
+    name: {type: String, trim: true, required: true},
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(val) {
+        if (!validator.isEmail(val)) throw new Error('Email is not valid');
+      },
     },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 7,
+      validate(val) {
+        if (val.toLowerCase().includes('password')) {
+          throw new Error("password can't contain 'password'");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      default: 100,
+      min: 18,
+      validate(val) {
+        if (val > 150) {
+          throw new Error("Stop playin'.");
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {type: String, required: true},
+      },
+    ],
   },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(val) {
-      if (val.toLowerCase().includes('password')) {
-        throw new Error("password can't contain 'password'");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 100,
-    min: 18,
-    validate(val) {
-      if (val > 150) {
-        throw new Error("Stop playin'.");
-      }
-    },
-  },
-  tokens: [
-    {
-      token: {type: String, required: true},
-    },
-  ],
-});
+  {timestamps: true}
+);
 
 userSchema.virtual('tasks', {
   ref: 'Task',
